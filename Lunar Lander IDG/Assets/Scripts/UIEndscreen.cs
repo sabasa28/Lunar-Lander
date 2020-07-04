b0a2fbf036e8d1fc16897f42a1b9f9dbf33de3ad
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2020-07-03 22:58:34 -0300

    Hecha base de Main Menu y Endscreen

---
+++ Lunar Lander IDG/Assets/Scripts/UIEndscreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIEndscreen : MonoBehaviour
+{
+    public void SwapToMainMenuScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void SwapToGameplayScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Lunar Lander IDG/Assets/Scripts/UIMainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMainMenu : MonoBehaviour
+{
+    public void SwapToGameplayScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
